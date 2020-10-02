975ed826b3e2252d33ed3badda4e1f1193c8d0cf
Sharad Chauhan <sharad@getinfino.com>
2020-10-02 21:57:55 +0530

    feat: core data managed object class

---
+++ MedReminder/Core Data/Model/MedicineHistory+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  MedicineHistory+CoreDataClass.swift
+//  MedReminder
+//
+//  Created by Sharad on 02/10/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(MedicineHistory)
+public class MedicineHistory: NSManagedObject {
+
+}
