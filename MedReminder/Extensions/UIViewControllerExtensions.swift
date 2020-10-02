f10bb43c003fe7a6cde97121702365e954cd4478
Sharad Chauhan <sharad@getinfino.com>
2020-10-02 21:59:58 +0530

    feat: UIViewController extension to show alerts

---
+++ MedReminder/Extensions/UIViewControllerExtensions.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewControllerExtensions.swift
+//  MedReminder
+//
+//  Created by Sharad on 02/10/20.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func showAlert(title: String, message: String, actionTitle: String, completion: (() -> Void)?) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: actionTitle, style: UIAlertAction.Style.cancel, handler: { _ in
+                completion?() ?? ()
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+}
